e68c441bdf818e4bfa9d2d95d1f285595607625a
Raj <rajmalave4@gmail.com>
2025-07-18 12:20:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://user-rewarder.vercel.app', 
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api', userRoutes);
